e64623bab51d8782e93271d3a14d19c37a95113c
Drishtdyumnss <drishtdyumn.shrivastava11@gmail.com>
2022-06-17 21:15:13 +0530

    connected frontend and backend

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Accordion, Badge, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import MainScreen from '../../components/MainScreen/MainScreen';
 import notes from '../../data/data';
+import axios from 'axios';
 
 const MyNotes = () => {
+  const [notes, setNotes] = useState();
+
+  const fetchNotes = async () => {
+    const { data } = await axios.get('/api/notes');
+    setNotes(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
   const deleteHandler = (id) => {};
+
   return (
     <MainScreen title='TITLE PLACE'>
       <Link to='/createnote'>
@@ -13,51 +27,52 @@ const MyNotes = () => {
           Create new Note
         </Button>
       </Link>
-      {notes.map((note) => (
-        <Accordion defaultActiveKey='0'>
-          <Card style={{ margin: 10 }}>
-            <Card.Header style={{ display: 'flex' }}>
-              <span
-                style={{
-                  color: 'black',
-                  textDecoration: 'none',
-                  flex: 1,
-                  cursor: 'pointer',
-                  alignSelf: 'center',
-                  fontSize: 18,
-                }}
-              >
-                {/* <Accordion.Toggle  as={Card.Text} variant='link' eventKey='0'> */}
-                {note.title}
-                {/* </Accordion.Toggle> */}
-              </span>
-
-              <div>
-                <Button href={`/note/${note._id}`}>Edit</Button>
-                <Button
-                  variant='danger'
-                  className='mx-2'
-                  onClick={() => deleteHandler(note._id)}
+      {notes &&
+        notes.map((note) => (
+          <Accordion defaultActiveKey='0'>
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: 'flex' }}>
+                <span
+                  style={{
+                    color: 'black',
+                    textDecoration: 'none',
+                    flex: 1,
+                    cursor: 'pointer',
+                    alignSelf: 'center',
+                    fontSize: 18,
+                  }}
                 >
-                  Delete
-                </Button>
-              </div>
-            </Card.Header>
-            <Accordion.Collapse eventKey='0'>
-              <Card.Body>
-                <h4>
-                  <Badge variant='success'>Category - {note.category}</Badge>
-                </h4>
+                  {/* <Accordion.Toggle  as={Card.Text} variant='link' eventKey='0'> */}
+                  {note.title}
+                  {/* </Accordion.Toggle> */}
+                </span>
 
-                <blockquote className='blockquote mb-0'>
-                  <p>{note.content}</p>
-                  <footer className='blockquote-footer'>Created on</footer>
-                </blockquote>
-              </Card.Body>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
-      ))}
+                <div>
+                  <Button href={`/note/${note._id}`}>Edit</Button>
+                  <Button
+                    variant='danger'
+                    className='mx-2'
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Card.Header>
+              <Accordion.Collapse eventKey='0'>
+                <Card.Body>
+                  <h4>
+                    <Badge variant='success'>Category - {note.category}</Badge>
+                  </h4>
+
+                  <blockquote className='blockquote mb-0'>
+                    <p>{note.content}</p>
+                    <footer className='blockquote-footer'>Created on</footer>
+                  </blockquote>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
+        ))}
     </MainScreen>
   );
 };
